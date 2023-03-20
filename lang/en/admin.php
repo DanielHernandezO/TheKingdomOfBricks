f0942007cee427d3d9e3ff88dcb18ee1c7cc2003
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2023-03-19 22:05:50 -0500

    [ADD] Basic layout for admin view and index page

---
+++ lang/en/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'The kingdom of the bricks',
+    'subtitle' => 'Welcome!',
+    'home' => 'Home',
+    'devs' => 'The KB devs',
+    'welcome' => 'Welcome to the admin view'
+];
