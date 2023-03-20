29451f7d5a9c6f7332d54889822c9b596baa0804
Jguerra47 <juan.guerra@mercadolibre.com.co>
2023-03-20 00:31:43 -0500

    [ADD] Routes from web file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/items/create', 'App\Http\Controllers\ItemController@create')->name('item.create');
+Route::post('/items/save', 'App\Http\Controllers\ItemController@save')->name('item.save');
+Route::delete('/items/delete/{id}', 'App\Http\Controllers\ItemController@delete')->name('item.delete');
+Route::get('/items', 'App\Http\Controllers\ItemController@index')->name('item.index');
+Route::get('/items/{id}', 'App\Http\Controllers\ItemController@show')->name('item.show');
