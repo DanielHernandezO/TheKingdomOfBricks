344373539efad29d10fffdf4546679a8389709b5
Jguerra47 <juan.guerra@mercadolibre.com.co>
2023-03-20 22:03:27 -0500

    [ADD] Middleware group assignment to routes

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
-Route::get('/admin', 'App\Http\Controllers\AdminController@index')->name('admin.index');
 
-Route::get('/admin/items/create', 'App\Http\Controllers\ItemController@create')->name('admin.item.create');
-Route::post('/admin/items/save', 'App\Http\Controllers\ItemController@save')->name('admin.item.save');
-Route::delete('/admin/items/delete/{id}', 'App\Http\Controllers\ItemController@delete')->name('admin.item.delete');
-Route::get('/admin/items', 'App\Http\Controllers\ItemController@index')->name('admin.item.index');
-Route::get('/admin/items/{id}', 'App\Http\Controllers\ItemController@show')->name('admin.item.show');
+/*
+|--------------------------------------------------------------------------
+| Here you will found all the routes related to admin role. This middleware
+| would accept incoming traffic from users with an admin role. Otherwise,
+| is going to be rejected due an unauthorized request.
+*/
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', 'App\Http\Controllers\AdminController@index')->name('admin.index');
 
-Route::get('/admin/reviews', 'App\Http\Controllers\ReviewController@index')->name('admin.review.index');
-Route::get('/admin/reviews/{id}', 'App\Http\Controllers\ReviewController@show')->name('admin.review.show');
-Route::delete('/admin/reviews/{id}', 'App\Http\Controllers\ReviewController@delete')->name('admin.review.delete');
+    Route::get('/admin/items/create', 'App\Http\Controllers\ItemController@create')->name('admin.item.create');
+    Route::post('/admin/items/save', 'App\Http\Controllers\ItemController@save')->name('admin.item.save');
+    Route::delete('/admin/items/delete/{id}', 'App\Http\Controllers\ItemController@delete')->name('admin.item.delete');
+    Route::get('/admin/items', 'App\Http\Controllers\ItemController@index')->name('admin.item.index');
+    Route::get('/admin/items/{id}', 'App\Http\Controllers\ItemController@show')->name('admin.item.show');
+
+    Route::get('/admin/reviews', 'App\Http\Controllers\ReviewController@index')->name('admin.review.index');
+    Route::get('/admin/reviews/{id}', 'App\Http\Controllers\ReviewController@show')->name('admin.review.show');
+    Route::delete('/admin/reviews/{id}', 'App\Http\Controllers\ReviewController@delete')->name('admin.review.delete');
+});
 
 Auth::routes();
