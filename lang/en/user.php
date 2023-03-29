4366f2a32206133ff44a69d7a25abcdebc43d1f1
DanielHernandezO <daniel.hernandez.54361@gmail.com>
2023-03-28 20:38:36 -0400

    [ADD] Added my purchases view and fixed lang

--- lang/en/user.php
+++ lang/en/user.php
@@ -1,6 +1,14 @@
 <?php
 
 return [
+    //layout
+    'items'=> 'Items',
+    'cart' => 'Cart',
+    'myPurchases'=>'My Purchases',
+    'login'=>'Login',
+    'register'=>'Register',
+    'logout'=>'Logout',
+
     //shop
     'itemTitle' => 'shop',
 
@@ -12,7 +20,11 @@ return [
     'success'=> 'Successful purchase!',
     'successPurchase'=>'Congratulations, your purchase has been successful!',
     'orderNumber'=>'Your order number is',
+    'orderId'=>'Order #',
     'oderPurchaseSection'=> 'Your order will be reflected in the \'My Purchases\' section.',
+    'date'=>'Date',
+    'totalPaid'=>'Total Paid',
+    'withNoPurchase'=>'Seems to be that you have not purchased anything in our store =(.',
 
     //actions
     'addToCart' => 'Add to cart',
---
+++ resources/views/user/purchase/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('content')
+    @forelse ($viewData["purchases"] as $order)
+        <div class="card mb-4">
+            <div class="card-header"><b>{{ __('commons.inputLabelWithValue', ['att' => __('user.orderId'), 'val' => $order->getId()])}}</b></div>
+            <div class="card-body">
+                {{ __('commons.inputLabelWithValue', ['att' => __('user.date'), 'val' => $order->getCreatedAt()])}}<br />
+                {{ __('commons.inputLabelWithValue', ['att' => __('user.totalPaid'), 'val' => $order->getTotalAmount()])}}<br />
+                <table class="table table-bordered table-striped text-center mt-3">
+                    <thead>
+                        <tr>
+                            <th scope="col">{{__('commons.id')}}</th>
+                            <th scope="col">{{__('commons.title')}}</th>
+                            <th scope="col">{{__('commons.price')}}</th>
+                            <th scope="col">{{__('user.quantity')}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($order->getOrderItem() as $orderItem)
+                            <tr>
+                                <td>{{ $orderItem->getId() }}</td>
+                                <td>
+                                    <a class="link-success" href="{{ route('user.item.show', ['id' => $orderItem->getItem()->getId()]) }}">
+                                        {{ $orderItem->getItem()->getTitle() }}
+                                    </a>
+                                </td>
+                                <td>${{ $orderItem->getPrice() }}</td>
+                                <td>{{ $orderItem->getQuantity() }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @empty
+        <div class="alert alert-danger" role="alert">
+            {{__('user.withNoPurchase')}}
+        </div>
+    @endforelse
+    <div class="d-flex justify-content-center">
+        {{ $viewData["purchases"]->render('pagination::bootstrap-4') }}
+    </div>
+@endsection
