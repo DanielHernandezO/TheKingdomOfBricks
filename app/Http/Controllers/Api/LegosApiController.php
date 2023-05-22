c3cea19364aa3f487e447b094dad9a6d3f4c0ec0
DanielHernandezO <daniel.hernandez.54361@gmail.com>
2023-05-22 13:09:08 -0500

    [ADD] items API

---
+++ app/Http/Controllers/Api/LegosApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ItemResource;
+use App\Models\Item;
+use Illuminate\Http\JsonResponse;
+
+class LegosApiController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $items = ItemResource::collection(Item::all());
+        return response()->json($items, 200);
+    }
+}
