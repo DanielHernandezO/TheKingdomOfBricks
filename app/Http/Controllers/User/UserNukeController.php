0d28bb05123326bebf4d32f75db436d36b257f76
Jguerra47 <jguerra47canada@gmail.com>
2023-05-23 00:29:25 -0500

    [ADD] Consume new clients

--- app/Http/Controllers/User/UserNukeController.php
+++ app/Http/Controllers/User/UserNukeController.php
@@ -4,17 +4,34 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\NukeService;
+use App\Interfaces\NukeImageService;
+use App\Interfaces\FlagImageService;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class UserNukeController extends Controller
 {
+    protected NukeService $nukeClient;
+    protected FlagImageService $flagImageClient;
+    protected NukeImageService $nukeImageClient;
+
+    public function __construct() {
+        $this->nukeClient = app(NukeService::class);
+        $this->flagImageClient = app(FlagImageService::class);
+        $this->nukeImageClient = app(NukeImageService::class);
+    }
     public function index(Request $request): View
     {
-        $nukeClient = app(NukeService::class);
-        $viewData = [];
-        $viewData['bombs'] = $nukeClient->getAllBombs();
+        $bombs = $this->nukeClient->getAllBombs();
+        $bombs = array_slice($bombs, 0, 10);
 
+        $bombs = collect($bombs)->map(function($bomb) {
+            $bomb['url'] = $this->nukeImageClient->getNuke($bomb['name']);
+            return $bomb;
+        });
+
+        $viewData = [];
+        $viewData['bombs'] = $bombs;
         return view('user.nuke.index')->with('viewData', $viewData);
     }
 }
