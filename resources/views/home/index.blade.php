51604dfcab34533d8539e5497d1af61776ceec2e
Jguerra47 <jguerra47canada@gmail.com>
2023-05-24 00:20:46 -0500

    [ADD] Redirect to index.php from public request

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,17 +8,11 @@
             <div class="col-md-6 mb-6">
                 <div class="card">
                     <img src="{{ asset('images/bowser.webp') }}" class="card-img-top" alt="Card 1">
-                    <div class="card-body">
-                        <a href="#" class="btn btn-primary"><i class="fas fa-info-circle"></i>{{__('commons.products')}}</a>
-                    </div>
                 </div>
             </div>
             <div class="col-md-6 mb-6">
                 <div class="card">
                     <img src="{{ asset('images/Ninjago.png') }}" class="card-img-top" alt="Card 2">
-                    <div class="card-body">
-                        <a href="#" class="btn btn-primary"><i class="fas fa-info-circle"></i>{{__('commons.about')}}</a>
-                    </div>
                 </div>
             </div>
         </div>
