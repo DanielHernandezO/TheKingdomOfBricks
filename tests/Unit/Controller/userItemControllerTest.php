fcf295e1a15faee8c204d12539f04b8d26370c1c
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2023-05-23 20:01:32 -0500

    [ADD] unit testing for controller and util

---
+++ tests/Unit/Controller/userItemControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class userItemControllerTest extends TestCase
+{   
+
+    use RefreshDatabase;
+    
+    public function testUserItemcontroller(): void
+    {   
+        //ACT
+        $response = $this->get('/items');
+
+        //ASSERT
+        $response->assertStatus(200)
+                 ->assertViewIs('user.item.index')
+                 ->assertViewHas('viewData.type')
+                 ->assertViewHas('viewData.items');
+    }
+}
